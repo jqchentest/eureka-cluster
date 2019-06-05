f6fd8fb8438d601699f41739cea06109fde41c1f
chenjunqiang <chenjunqiang@aixhc.com>
2019-06-05 10:39:35 +0800

    init

--- src/main/java/com/jqchen/EurekaApplication.java
+++ src/main/java/com/jqchen/EurekaApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurakeLocalApplication {
+public class EurekaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurakeLocalApplication.class, args);
+        SpringApplication.run(EurekaApplication.class, args);
     }
 
 }
